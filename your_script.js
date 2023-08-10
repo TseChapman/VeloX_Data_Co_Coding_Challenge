71a0fc94ba3c6b701cbc3bc08f7fca46bbd50be6
TseChapman <tch222000@gmail.com>
2023-08-10 16:22:40 -0700

    Implement error handling for not exist file in readFileByFileName

--- your_script.js
+++ your_script.js
@@ -1,9 +1,13 @@
 // Helper function to read a file by file name
 async function readFileByFileName(filePath) {
+  try {
     const fs = require('fs').promises;
     const EMPTY_CHAR = /\s+/g;
     const fileContents = await fs.readFile(filePath, 'utf-8');
     return fileContents.replace(EMPTY_CHAR, '');
+  } catch (error) {
+    throw new Error('readFileByFileName: File not found');
+  }
 }
 
 module.exports = {
