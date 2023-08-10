1f3d2dc6b9b5f85a9e914b5e194dbc8c82e9d093
TseChapman <tch222000@gmail.com>
2023-08-10 16:25:49 -0700

    Implementinvalid file path input in readFileByFileName function

--- your_script.js
+++ your_script.js
@@ -1,5 +1,9 @@
 // Helper function to read a file by file name
 async function readFileByFileName(filePath) {
+  if (filePath === null || typeof(filePath) !== 'string') {
+    throw new Error('readFileByFileName: Expect string from filePath');
+  }
+
   try {
     const fs = require('fs').promises;
     const EMPTY_CHAR = /\s+/g;
