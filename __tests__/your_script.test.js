1a01a7d61669aa5a25cc04f03ac7de2572c23aa9
TseChapman <tch222000@gmail.com>
2023-08-10 16:45:59 -0700

    Implement step1 function that successfully count the unique elements and sort them

--- __tests__/your_script.test.js
+++ __tests__/your_script.test.js
@@ -1,6 +1,7 @@
 const {
   readFileByFileName,
   validateElementsString,
+  step1,
 } = require('../your_script');
 
 const fs = require('fs').promises;
--- your_script.js
+++ your_script.js
@@ -25,7 +25,43 @@ function validateElementsString(elementsString) {
   return elements.filter(element => VALID_ELEMENT_REGEX.test(element));
 }
 
+// Step 1: Count unique elements
+async function step1(elements) {
+  const counts = {};
+
+  for (const element of elements) {
+    counts[element] = (counts[element] || 0) + 1;
+  }
+
+  // Sort the counts by key, starting from number to text
+  const sortedKeys = Object.keys(counts).sort((a, b) => {
+    const aIsNumber = !isNaN(a);
+    const bIsNumber = !isNaN(b);
+
+    if (aIsNumber && bIsNumber) {
+      return a - b;
+    } else if (aIsNumber) {
+      return -1;
+    } else if (bIsNumber) {
+      return 1;
+    } else {
+      return a.localeCompare(b);
+    }
+  });
+
+  // create a sorted counts and return it
+  const sortedCounts = {};
+  for (const key of sortedKeys) {
+    sortedCounts[key] = counts[key];
+  }
+
+  console.log("Step 1 Result: ", JSON.stringify(sortedCounts, null, 2));
+  console.log("==============================================");
+  return sortedCounts;
+}
+
 module.exports = {
   readFileByFileName,
   validateElementsString,
+  step1,
 }
